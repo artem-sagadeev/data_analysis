f3fe0e578d7afe14b7e24494ff80401e8dc854da
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-09-17 18:32:32 +0300

    first hw

---
+++ DataAnalysis.Hw1/DataAnalysis.Hw1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DataAnalysis.Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var y = File
+                .ReadAllLines("lines.csv")
+                .Select(e => double.Parse(e.Split(";")[7].Replace(".", ",")))
+                .ToList();
+            var i = 0;
+            var x = y.Select(_ => i++).ToList();
+            var newX = x.Sum() / x.Count;
+            var newY = y.Sum() / y.Count;
+            var xy = 0d;
+            for (var j = 0; j < y.Count; j++)
+            {
+                xy += x[j] * y[j];
+            }
+
+            var x2 = x.Select(e => e * e).Sum() / x.Count;
+            var a = (xy - newX * newY) / (x2 - newX * newX);
+            var b = newY - a * newX;
+            Console.WriteLine($"a = {a}, b = {b}");
+            var answers = new List<double>();
+            for (var j = 0; j < x.Count; j++)
+            {
+                answers.Add(a * x[j] + b <= y[j] ? 1 : -1);
+            }
+
+            i = 0;
+            var inputs = y.Select(e => new double[] { i, y[i++] }).ToArray();
+            foreach (var e in x)
+            {
+                Console.Write(e + " ");
+            }
+            foreach (var e in y)
+            {
+                Console.Write(e + " ");
+            }
+
+            var model = new SinglePerceptron(2, 3134.8901260195853);
+            model.Fit(inputs, answers.ToArray());
+        }
+    }
+}
---
+++ DataAnalysis.Hw1/DataAnalysis.Hw1/SinglePerceptron.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAnalysis.Hw1
+{
+    public class SinglePerceptron
+    {
+        private readonly int _inputsCount;
+        private readonly double[] _weights;
+
+        public SinglePerceptron(int inputsCount, double delta)
+        {
+            _inputsCount = inputsCount;
+            _weights = new double[inputsCount];
+            var random = new Random();
+            for (var i = 0; i < inputsCount; i++)
+            {
+                _weights[i] = 1;
+            }
+        }
+
+        private int Predict(double[] input)
+        {
+            if (input.Length != _inputsCount)
+                throw new ArgumentException();
+
+            var sum = 0d;
+            for (var i = 0; i < _inputsCount; i++)
+            {
+                sum += _weights[i] * input[i];
+            }
+
+            return sum > 0 ? 1 : -1;
+        }
+
+        public void Fit(double[][] input, double[] output)
+        {
+            var errs = -1;
+            var minErrs = input.Length;
+            while (errs != 0)
+            {
+                errs = 0;
+                for (var i = 0; i < input.Length; i++)
+                {
+                    var row = input[i];
+                    var prediction = Predict(row);
+                    if (prediction * output[i] < 0)
+                    {
+                        errs += 1;
+                        _weights[0] += 0.001d * row[0] * output[i];
+                    }
+                }
+                var temp = minErrs;
+                minErrs = Math.Min(errs, minErrs);
+                if (temp != minErrs)
+                {
+                    Console.WriteLine(minErrs);
+                    foreach (var weight in _weights)
+                    {
+                        Console.Write(weight + " ");
+                    }
+                }
+            }
+        }
+    }
+}
