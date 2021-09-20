4307b81ef58588833c1d9f396b5bbcfbb9aaf8c5
artyom.sagadeev <artyom.sagadeev@kzn.akvelon.com>
2021-09-20 20:32:47 +0300

    done with hw2

---
+++ DataAnalysis.Hw2/DataAnalysis.Hw2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DataAnalysis.Hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = InputData();
+            var answers = lines.Select(line => line[0] == 4 ? 1 : 0).ToArray();
+
+            for (var i = 1; i <= 4; i++)
+            {
+                var inputs = lines.Select(line => line.Skip(1).Take(i).ToArray()).ToArray();
+                var perceptron = new SinglePerceptron(i, 0.000001d);
+                perceptron.Fit(inputs, answers);
+            }
+        }
+
+        private static int[][] InputData()
+            => File
+                .ReadAllLines("../../../vk_perc.csv")
+                .Select(e => e
+                    .Split(';')
+                    .Select(int.Parse)
+                    .ToArray())
+                .ToArray();
+    }
+}
---
+++ DataAnalysis.Hw2/DataAnalysis.Hw2/SinglePerceptron.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace DataAnalysis.Hw2
+{
+    public class SinglePerceptron
+    {
+        private readonly int _inputsCount;
+        private readonly double[] _weights;
+        private readonly double _tempo;
+
+        public SinglePerceptron(int inputsCount, double tempo)
+        {
+            _inputsCount = inputsCount;
+            _tempo = tempo;
+
+            var random = new Random();
+            _weights = Enumerable
+                .Range(0, inputsCount)
+                .Select(_ => random.NextDouble())
+                .ToArray();
+        }
+
+        private int Predict(int[] input)
+        {
+            var sum = 0d;
+            for (var i = 0; i < _inputsCount; i++)
+                sum += _weights[i] * input[i];
+
+            return sum > 0.5 ? 1 : 0;
+        }
+
+        public void Fit(int[][] inputs, int[] answers)
+        {
+            var age = 0;
+            var minErrorsCount = inputs.Length;
+            while (age < 100)
+            {
+                var errorsCount = 0;
+                for (var i = 0; i < inputs.Length; i++)
+                {
+                    var input = inputs[i];
+                    var prediction = Predict(input);
+                    if (prediction != answers[i])
+                    {
+                        errorsCount += 1;
+                    }
+                    for (var j = 0; j < _inputsCount; j++)
+                    {
+                        _weights[j] += _tempo * input[j] * (answers[i] - prediction);
+                    }
+                }
+
+                minErrorsCount = Math.Min(errorsCount, minErrorsCount);
+                age++;
+            }
+            
+            Console.WriteLine($"Input count: {_inputsCount}, right answers percent: {100 - minErrorsCount * 100 / 720}%");
+            Console.Write($"Weights: ");
+            foreach (var weight in _weights)
+            {
+                Console.Write(weight + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
