8f24a9e1dd2ab044c90b1f30038fe8a8ae56ba5b
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-12-21 13:56:12 +0300

    fixes

---
+++ DataAnalysis.Hw3/DataAnalysis.Hw3/DataTool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAnalysis.Hw3
+{
+    public static class DataTool
+    {
+        public static IEnumerable<double[]> PrepareInput(this double[] data)
+        {
+            for (var i = 0; i < data.Length - 13; i++)
+            {
+                yield return data.Skip(i).Take(10).ToArray();
+            }
+        }
+        
+        public static IEnumerable<double> PrepareAnswers(this double[] data)
+        {
+            for (var i = 10; i < data.Length - 3; i++)
+            {
+                yield return GetTrendDirection(
+                    data.Skip(i).Take(3).Select((_, x) => x).ToList(),
+                    data.Skip(i).Take(3).Select((y, _) => y).ToList());
+            }
+        }
+
+        private static double GetTrendDirection(List<int> x, List<double> y)
+        {
+            var xAverage = x.Average();
+            var yAverage = y.Average();
+            var xyAverage = y.Select((t, i) => x[i] * t).Sum() / y.Count;
+            var x2 = x.Select(e => e * e).Sum() / x.Count;
+            return (xyAverage - xAverage * yAverage) / (x2 - xAverage * xAverage) > 0 ? 1 : 0;
+        }
+    }
+}
--- DataAnalysis.Hw3/DataAnalysis.Hw3/Program.cs
+++ DataAnalysis.Hw3/DataAnalysis.Hw3/Program.cs
@@ -10,12 +10,12 @@ namespace DataAnalysis.Hw3
         static void Main(string[] args)
         {
             var educationalData = InputData("../../../EducationalData.csv");
-            var educationalInputs = PrepareInput(educationalData).ToArray();
-            var educationalAnswers = PrepareAnswers(educationalData).ToArray();
+            var educationalInputs = educationalData.PrepareInput().ToArray();
+            var educationalAnswers = educationalData.PrepareAnswers().ToArray();
             
             var testData = InputData("../../../TestData.csv");
-            var testInputs = PrepareInput(testData).ToArray();
-            var testAnswers = PrepareAnswers(testData).ToArray();
+            var testInputs = testData.PrepareInput().ToArray();
+            var testAnswers = testData.PrepareAnswers().ToArray();
             
             var perceptron = new DoublePerceptron(10, 0.0001d, 5000);
             perceptron.Study(educationalInputs, educationalAnswers);
@@ -31,32 +31,5 @@ namespace DataAnalysis.Hw3
                 .Select(line => line.Split(';')[7])
                 .Select(double.Parse)
                 .ToArray();
-
-        static IEnumerable<double[]> PrepareInput(double[] data)
-        {
-            for (var i = 0; i < data.Length - 13; i++)
-            {
-                yield return data.Skip(i).Take(10).ToArray();
-            }
-        }
-        
-        static IEnumerable<double> PrepareAnswers(this double[] data)
-        {
-            for (var i = 10; i < data.Length - 3; i++)
-            {
-                yield return GetTrendDirection(
-                    data.Skip(i).Take(3).Select((_, x) => x).ToList(),
-                    data.Skip(i).Take(3).Select((y, _) => y).ToList());
-            }
-        }
-
-        static double GetTrendDirection(List<int> x, List<double> y)
-        {
-            var xAverage = x.Average();
-            var yAverage = y.Average();
-            var xyAverage = y.Select((t, i) => x[i] * t).Sum() / y.Count;
-            var x2 = x.Select(e => e * e).Sum() / x.Count;
-            return (xyAverage - xAverage * yAverage) / (x2 - xAverage * xAverage) > 0 ? 1 : 0;
-        }
     }
 }
